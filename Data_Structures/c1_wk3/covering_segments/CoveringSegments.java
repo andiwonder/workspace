de8bb21c4454e1894248382fe577f98f4e7e1eaf
Swapnil Kasliwal <skasliwal12@gmail.com>
2016-09-25 19:00:26 -0400

    side tracked

--- Data_Structures/c1_wk3/covering_segments/CoveringSegments.java
+++ Data_Structures/c1_wk3/covering_segments/CoveringSegments.java
@@ -1,17 +1,9 @@
+package covering_segments;
 import java.util.*;
 
 public class CoveringSegments {
-
-    private static int[] optimalPoints(Segment[] segments) {
-        //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
-    }
-
+	
+	
     private static class Segment {
         int start, end;
 
@@ -19,7 +11,43 @@ public class CoveringSegments {
             this.start = start;
             this.end = end;
         }
+        
+        public int  getEnd() {
+            return this.end;
+        }
+        
+        public int  getStart() {
+            return this.start;
+        }        
+
+        public String toString() {
+            return getStart() + ", " + getEnd() + "|";
+        }
     }
+
+    private static int[] optimalPoints(Segment[] segments) {
+        //write your code here
+//        int[] points = new int[2 * segments.length];
+        ArrayList<Integer> points2 = new ArrayList<Integer>();
+        Arrays.sort(segments, new Comparator<Segment>() {
+            public int compare(Segment o1, Segment o2) {
+            	return Integer.compare(o1.getEnd(), o2.getEnd());
+            }
+        });
+        int compare_point = segments[0].getEnd();
+        for (int i = 0; i < segments.length; i++) {
+        	if (segments[i].getStart() <= compare_point && compare_point <= segments[i].getEnd()){
+        		
+        	} else {
+        		points2.add(compare_point);
+        		compare_point = segments[i].getEnd();
+        	}
+        }
+        
+        System.out.println("points length is " + points2.size);
+        return points;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
--- Data_Structures/c1_wk3/fractional_knapsack/FractionalKnapsack.java
+++ Data_Structures/c1_wk3/fractional_knapsack/FractionalKnapsack.java
@@ -1,88 +1,48 @@
-package fractional_knapsack;
-//import java.util.Scanner;
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.TreeMap;
-//import java.util.Set;
-//import java.util.Iterator;
-//import java.util.LinkedHashMap;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Map;
+//package fractional_knapsack;
 import java.util.*;
 
 
 public class FractionalKnapsack {
-	
-//	static class Pair implements Comparable<Pair> {
-//	    public final int index;
-//	    public final double value;
-//
-//	    public Pair(int index, double value) {
-//	        this.index = index;
-//	        this.value = value;
-//	    }
-//
-//	    @Override
-//	    public int compareTo(Pair other) {
-//	        //multiplied to -1 as the author need descending sort order
-//	        return -1 * Double.valueOf(this.value).compareTo(other.value);
-//	    }
-//	}
-	
-	 public static Map<Integer, Double> sortByValue(Map<Integer, Double> map) {
-	        List list = new LinkedList(map.entrySet());
-	        Collections.sort(list, new Comparator() {
+	public static Map<Integer, Double> sortByValue(Map<Integer, Double> map) {
+		List list = new LinkedList(map.entrySet());
+	    Collections.sort(list, new Comparator() {
 
-	            @Override
-	            public int compare(Object o1, Object o2) {
-	                return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
-	            }
-	        });
+	    	@Override
+	    	public int compare(Object o1, Object o2) {
+	    		return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
+	    	}
+	    });
 
-	        Map result = new LinkedHashMap();
-	        for (Iterator it = list.iterator(); it.hasNext();) {
-	            Map.Entry entry = (Map.Entry) it.next();
+	    Map result = new LinkedHashMap();
+	    	for (Iterator it = list.iterator(); it.hasNext();) {
+	    		Map.Entry entry = (Map.Entry) it.next();
 	            result.put(entry.getKey(), entry.getValue());
 	        }
-	        return result;
-	    }
+	    return result;
+	 }
 	
 	private static double getOptimalValue(int capacity, int[] values, int[] weights) {
 		double value = 0;
-//        TreeMap<Double, Integer> tmap = new TreeMap<Double, Integer>(Collections.reverseOrder());
-//        Pair[] yourArray = new Pair[values.length];
-		 HashMap<Integer, Double> mapa = new HashMap<Integer, Double>();
-		 
-        //write your code here
+		HashMap<Integer, Double> mapa = new HashMap<Integer, Double>();
         for (int i = 0; i < values.length; i++) {
-        		double ratio =  values[i] / weights[i];  
-        		mapa.put(values[i], ratio);
-//        		tmap.put(ratio, i);
-//        		yourArray[i] = new Pair(5, ratio); 
+        		double ratio = (double)  values[i] / weights[i];
+        		mapa.put(i, ratio);
         }
-        for (HashMap.Entry<Integer, Double> map : mapa.entrySet()) {
-            System.out.println(map.getKey() + " : " + map.getValue());
-        };
-//        Set set = tmap.entrySet();
-//        Arrays.sort(yourArray);
-//        System.out.println(yourArray[0]);
-//        System.out.println(yourArray[1]);
-//        Iterator iterator = set.iterator();
-//        Boolean full = false;
-//        while(full == false) {
-//           Map.Entry mentry = (Map.Entry)iterator.next();
-//           System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-//           System.out.println(mentry.getValue());
-//           capacity = capacity - weights[(int) mentry.getValue()];
-//           value += values[(int) mentry.getValue()]; 
-//           System.out.println("Capacity is now " + capacity);
-//           System.out.println("Value is now " + value);
-//           if (capacity <= 0){
-//        	     full = true;
-//           }
-//         
-//        }
+        Map<Integer, Double> sortedMap = FractionalKnapsack.sortByValue(mapa);
+        Iterator it = sortedMap.entrySet().iterator();
+        boolean empty = false;
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (capacity - weights[(int) pair.getKey()] <= 0){
+            	double remWeightRatio = (double) capacity / weights[(int) pair.getKey()];
+            	value = value + (remWeightRatio * values[(int) pair.getKey()]);
+            	break;
+            } else {
+            	capacity = capacity - weights[(int) pair.getKey()];
+                value = value + values[(int) pair.getKey()];
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
         return value;
     }
     
